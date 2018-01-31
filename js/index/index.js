b432b2682e99409fd1634a2824d16797c5dec329
wanghao <1750110604@qq.com>
2018-01-31 21:37:09 +0800

    意见反馈

--- js/index/index.js
+++ js/index/index.js
@@ -9,11 +9,43 @@ $(function(){
 		}
 		
 	})
-	//menu
+	/**
+	 *菜单
+	 */
 	$('#logo-btn').on('click',function(){
 		$(".menu-frame").load("template/menu.html");
 	})
 	$('.menu-frame').on('click','.menu',function(){
 		$(this).remove();
 	})
+
+	 /*******************-----意见反馈-----******************/
+	 //打开
+	$('.menu-frame').on('click','#menu_news',function(){
+		$(".menu-list-frame").load("template/menu-Opinion.html");
+	})
+	//关闭
+	$('.menu-list-frame').on('click','#close',function(){
+		$('.menu-list-frame').html('');
+	});
+	//取消
+	$('.menu-list-frame').on('click','#cancer',function(){
+		$('.menu-list-frame').html('');
+	});
+	//确定
+	$('.menu-list-frame').on('click','#query',function(){
+		$('.menu-list-frame').html('');
+	});
+	//监听输入
+	$('.menu-list-frame').on('input propertychange','#textarea',function(){
+		var value = $(this).val().length;
+		$('.menu-list-news #wordsNumber').html(value);
+	})
+
+
+
+
+
+
+
 })

a883e4e7e647f8fb7d6bee1c7f0264bca000ea6f
wanghao12345 <1750110604@qq.com>
2018-02-05 20:43:48 +0800

    update

--- js/game/websocket.js
+++ js/game/websocket.js
@@ -29,4 +29,26 @@ var closeSocket = function(){
 }
 
 
-
+/**
+ * 获取url中参数
+ */
+function getUrlParam(param){
+    var reg = new RegExp("(^|&)" + param + "=([^&]*)(&|$)", "i"); 
+    var r = window.location.search.substr(1).match(reg); 
+    if (r != null) return unescape(r[2]); 
+    return null; 
+}
+/**
+ * 获取当前手机的系统类型
+ */
+function getMobileType(){
+	var u = navigator.userAgent;
+	var device =""; //当前设备信息
+	if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {//安卓手机
+	    return 1;
+	} else if (u.indexOf('iPhone') > -1) {//苹果手机
+	    return 2;
+	}else{
+		return 1;
+	}
+}

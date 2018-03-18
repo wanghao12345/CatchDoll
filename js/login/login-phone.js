4509d96abbc540001101ca4cbced8d274bac87c0
wanghao <1750110604@qq.com>
2018-03-18 15:30:52 +0800

    登录

---
+++ js/login/login-phone.js
@@ -0,0 +1,12 @@
+$(function(){
+	//点击获取验证码按钮
+	$('button#verification-btn').on('click',function(){
+		$(this).css('display','none');
+		$('.verification span#CountDown').css('display','block');
+	})
+
+
+
+
+
+})

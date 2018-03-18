7c996a3f59f9c0cf8e0e65645beda328fdc96c2a
wanghao <1750110604@qq.com>
2018-03-18 17:51:23 +0800

    登录2

--- js/login/login-phone.js
+++ js/login/login-phone.js
@@ -3,6 +3,7 @@ $(function(){
 	$('button#verification-btn').on('click',function(){
 		$(this).css('display','none');
 		$('.verification span#CountDown').css('display','block');
+		countDownTime60();
 	})
 
 
@@ -10,3 +11,19 @@ $(function(){
 
 
 })
+/**
+ *倒计时1分钟
+ */
+function countDownTime60(){
+	var num = 59;
+	var time = window.setInterval(function(){
+		num = num - 1;
+		if (num==-1) {//倒计时结束
+			$('button#verification-btn').css('display','block');
+			$('.verification span#CountDown').css('display','none');
+			window.clearInterval(time);
+		} else {
+			$('.verification span#CountDown').html(num+'S');		
+		}
+	},1000)
+}

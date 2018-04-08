b6c823c41e7702fbe92932b697621eb37966cb9b
wanghao <1750110604@qq.com>
2018-04-08 20:49:13 +0800

    update

--- js/index/ajaxRequest.js
+++ js/index/ajaxRequest.js
@@ -42,10 +42,18 @@ function login_phone(){
 	  dataType: 'json',
 	  success: function (data) {
 	    console.log(data)
-	    token = data.ret[0].d.token;
-	    guestno = data.ret[0].d.guestno;
-	    RequestMachineList(0);
-	    getUserInfo(data,'ish');
+	    if (data.ret[0].d.errcode==0) {
+		    token = data.ret[0].d.token;
+		    guestno = data.ret[0].d.guestno;
+		    RequestMachineList(0);
+		    getUserInfo(data,'ish');
+	    } else {
+	    	delCookie('login_phone_code');
+	    	delCookie('login_phone_qpwd');
+	    	window.history.go(-1);
+	    }
+
+
 	  },
 	  fail: function (err) {
 	    console.log(err)
@@ -1153,3 +1161,39 @@ function getUrlParam(param){
     if (r != null) return unescape(r[2]); 
     return null; 
 }
+
+/**
+ * 存储cookie
+ * setCookie('username','Darren',30)  
+ */ 
+function setCookie(c_name, value, expiredays){  
+　　var exdate=new Date();  
+　　exdate.setDate(exdate.getDate() + expiredays);  
+　　document.cookie=c_name+ "=" + escape(value) + ((expiredays==null) ? "" : ";expires="+exdate.toGMTString());  
+}  
+/**
+ * 获取cookie
+ * getCookie("username")
+ */
+function getCookie(name)  {  
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");  
+    if(arr=document.cookie.match(reg))  
+        return (arr[2]);  
+    else  
+        return null;  
+}  
+/**
+ * 删除cookie
+ */
+function delCookie(name)  {  
+    var exp = new Date();  
+    exp.setTime(exp.getTime() - 1);  
+    var cval=getCookie(name);  
+    if(cval!=null)  
+        document.cookie= name + "="+cval+";expires="+exp.toGMTString();  
+}
+
+
+
+
+
